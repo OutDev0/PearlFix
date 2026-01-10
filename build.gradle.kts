9cd7cfb2550c0a89cc50ebd310dcff519676f416
OutDev <lukassinah@icloud.com>
2026-01-10 19:22:11 +0100

    Setting up the project

---
+++ build.gradle.kts
@@ -0,0 +1,100 @@
+/*
+ *
+ *  * This file is part of PearlFix - https://github.com/OutDev0/PearlFix
+ *  * Copyright © 2026 OutDev and contributors
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+plugins {
+    `java-library`
+    `maven-publish`
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.run.paper)
+}
+
+group = "de.outdev.pearlfix"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(libs.paper.api)
+    compileOnly(libs.lombok)
+    compileOnly(libs.configlib.yaml)
+    compileOnly(libs.configlib.paper)
+
+    annotationProcessor(libs.lombok)
+}
+
+tasks {
+
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveFileName = "${rootProject.name}-${project.version}.jar"
+        archiveClassifier = null
+
+        manifest {
+            attributes["Implementation-Version"] = rootProject.version
+        }
+    }
+
+    assemble {
+        dependsOn(shadowJar)
+    }
+
+    withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release = 21
+    }
+
+    withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    defaultTasks("build")
+
+    // 1.8.8 - 1.16.5 = Java 8
+    // 1.17           = Java 16
+    // 1.18 - 1.20.4  = Java 17
+    // 1-20.5+        = Java 21
+    val version = "1.21.4"
+    val javaVersion = JavaLanguageVersion.of(21)
+
+    val jvmArgsExternal = listOf(
+        "-Dcom.mojang.eula.agree=true"
+    )
+
+    runServer {
+        minecraftVersion(version)
+        runDirectory = rootDir.resolve("run/paper/$version")
+
+        javaLauncher = project.javaToolchains.launcherFor {
+            languageVersion = javaVersion
+        }
+
+        downloadPlugins {
+            // None yet
+        }
+        jvmArgs = jvmArgsExternal
+    }
+}
