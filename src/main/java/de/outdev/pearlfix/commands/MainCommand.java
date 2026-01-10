e04191adf02b4bc4093bfc4b2f12d0a1f25eaafb
OutDev <lukassinah@icloud.com>
2026-01-10 23:01:57 +0100

    Added: Plugin command

---
+++ src/main/java/de/outdev/pearlfix/commands/MainCommand.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of PearlFix - https://github.com/OutDev0/PearlFix
+ * Copyright © 2026 OutDev and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package de.outdev.pearlfix.commands;
+
+import de.outdev.pearlfix.PearlFix;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MainCommand implements CommandExecutor, TabCompleter {
+
+    private final PearlFix plugin;
+
+    public MainCommand(PearlFix plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!sender.hasPermission("pearlfix.reload")) {
+            sender.sendMessage(Component.text("You do not have permission to execute this command!", NamedTextColor.RED));
+            return true;
+        }
+        if (args.length != 1 || !args[0].equalsIgnoreCase("reload")) {
+            sender.sendMessage(Component.text("Usage: /pearlfix reload", NamedTextColor.RED));
+            return true;
+        }
+
+        try {
+            plugin.getConfigManager().reload();
+            sender.sendMessage(Component.text("Successfully reloaded!", NamedTextColor.GREEN));
+            return true;
+        } catch (Exception e) {
+            sender.sendMessage(Component.text("Failed to reload.", NamedTextColor.RED));
+        }
+
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return List.of("reload");
+    }
+}
